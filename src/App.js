5c0faa73b2a68a0013ee9e81bde60f2bc7a30aba
Andrei Malets <coolviktori@yandex.by>
2023-03-26 01:23:23 +0300

    changes Swiger

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import layerBack from './assets/img/screen-1/layer-back.jpg'
 import layerMiddle from './assets/img/screen-1/layer-middle.png'
 import layerFront from './assets/img/screen-1/layer-front.png'
 
-import layerFront from './assets/img/screen-2/layer-back.jpg'
-import layerFront from './assets/img/screen-2/layer-gera.png'
-import layerFront from './assets/img/screen-2/layer-yen.png'
-import layerFront from './assets/img/screen-2/layer-front.png'
+import layerBackSecond from './assets/img/screen-2/layer-back.jpg'
+import layerGera from './assets/img/screen-2/layer-gera.png'
+import layerYen from './assets/img/screen-2/layer-yen.png'
+import layerFrontSecond from './assets/img/screen-2/layer-front.png'
 
-import layerFront from './assets/img/screen-1/layer-front.png'
-import layerFront from './assets/img/screen-1/layer-front.png'
-import layerFront from './assets/img/screen-1/layer-front.png'
-import layerFront from './assets/img/screen-1/layer-front.png'
+import layerBackThird from './assets/img/screen-3/layer-back.jpg'
+import layerMiddleThird from './assets/img/screen-3/layer-middle.png'
+import layerCiri from './assets/img/screen-3/layer-ciri.png'
+import layerFrontThird from './assets/img/screen-3/layer-front.png'
 
 
 
@@ -67,21 +67,21 @@ function App() {
             
           <SwiperSlide>
             <div className="slider__item">
-              <div className="slider__layer" data-swiper-parallax="40%" style="background-image: url(img/screen-2/layer-back.jpg);"></div>
-              <div className="slider__layer" data-swiper-parallax="34%" style="background-image: url(img/screen-2/layer-gera.png);"></div>
-              <div className="slider__layer" data-swiper-parallax="30%" style="background-image: url(img/screen-2/layer-yen.png);"></div>
+              <div className="slider__layer" data-swiper-parallax="40%" style={{backgroundImage: `${url({layerBackSecond})}}`}}></div>
+              <div className="slider__layer" data-swiper-parallax="34%" style={{backgroundImage: `${url({layerGera})}}`}}></div>
+              <div className="slider__layer" data-swiper-parallax="30%" style={{backgroundImage: `${url({layerYen})}}`}}></div>
               <canvas className="slider__layer" data-swiper-parallax="20%" data-color="#525A6D"></canvas>
-              <div className="slider__layer" data-swiper-parallax="14%" style="background-image: url(img/screen-2/layer-front.png);"></div>
+              <div className="slider__layer" data-swiper-parallax="14%" style={{backgroundImage: `${url({layerFrontSecond})}}`}}></div>
             </div>
           </SwiperSlide>
             
           <SwiperSlide>
             <div className="slider__item">
-              <div className="slider__layer" data-swiper-parallax="48%" style="background-image: url(img/screen-3/layer-back.jpg);"></div>
-              <div className="slider__layer" data-swiper-parallax="35%" style="background-image: url(img/screen-3/layer-middle.png);"></div>
-              <div className="slider__layer" data-swiper-parallax="29%" style="background-image: url(img/screen-3/layer-ciri.png);"></div>
+              <div className="slider__layer" data-swiper-parallax="48%" style={{backgroundImage: `${url({layerBackThird})}}`}}></div>
+              <div className="slider__layer" data-swiper-parallax="35%" style={{backgroundImage: `${url({layerMiddleThird})}}`}}></div>
+              <div className="slider__layer" data-swiper-parallax="29%" style={{backgroundImage: `${url({layerCiri})}}`}}></div>
               <canvas className="particles slider__layer" data-swiper-parallax="25%" data-color="#938086"></canvas>
-              <div className="slider__layer" data-swiper-parallax="16%" style="background-image: url(img/screen-3/layer-front.png);"></div>
+              <div className="slider__layer" data-swiper-parallax="16%" style={{backgroundImage: `${url({layerFrontThird})}}`}}></div>
             </div>
           </SwiperSlide>
             
