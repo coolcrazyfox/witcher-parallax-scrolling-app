f26f78633250abe69147ec5cf1dd3bf243bf3072
Andrei Malets <coolviktori@yandex.by>
2023-03-28 23:20:23 +0300

    test

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,51 +1,61 @@
-import React from 'react'
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import '../styles/App.css'
+import "../styles/App.css";
 import { Parallax, Pagination, Navigation } from "swiper";
 
-const Slider = ({slides}) => {
+const Slider = ({ slides }) => {
   return (
     <Swiper
-        spaceBetween={750}        
-        style={{
-          "--swiper-navigation-color": "red",
-          "--swiper-pagination-color": "red",
-        }}
-        speed={500}
-        parallax={true}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Parallax, Pagination, Navigation]}
-        className="slider"
-      >
-      {slides.map((s)=>(
+      spaceBetween={750}
+      style={{
+        "--swiper-navigation-color": "red",
+        "--swiper-pagination-color": "red",
+      }}
+      speed={500}
+      parallax={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Parallax, Pagination, Navigation]}
+      className="slider"
+    >
+      {slides.map((s) => (
         <SwiperSlide key={s.id}>
-          <div className="slider__item">      
-            <img data-swiper-parallax={s.firstData} 
-              src={s.back}              
-              className="slider__layer" />
-            <img data-swiper-parallax={s.secondData}
-              src={s.center}/>
-             <img data-swiper-parallax={s.testData}
-              src={s.test}             
-              className="slider__layer"/>
-            <canvas className="slider__layer" data-swiper-parallax={s.canvas} data-color={s.color}></canvas>  
-            <img data-swiper-parallax={s.thirdData}
-              src={s.front}            
-              className="slider__layer"/>               
-          </div>          
+          <div className="slider__item">
+            <img
+              data-swiper-parallax={s.firstData}
+              src={s.back}
+              className="slider__layer"
+            />
+            <img data-swiper-parallax={s.secondData} src={s.center} />
+            <img
+              data-swiper-parallax={s.testData}
+              src={s.test}
+              className="slider__layer"
+            />
+            <canvas
+              className="slider__layer"
+              data-swiper-parallax={s.canvas}
+              data-color={s.color}
+            ></canvas>
+            <img
+              data-swiper-parallax={s.thirdData}
+              src={s.front}
+              className="slider__layer"
+            />
+          </div>
+          <ul>
+            <li>2</li>
+            <li>1</li>
+          </ul>
         </SwiperSlide>
-      )
-
-      )}        
-        
+      ))}
     </Swiper>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
